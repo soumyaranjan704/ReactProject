72f3678833eeeba0f9c25dadb80758e097d98d1b
soumyaranjan704 <57033716+soumyaranjan704@users.noreply.github.com>
2022-10-07 11:03:02 +0530

    employee crud operation added

---
+++ Projects/localstorage-employee/src/hooks/useForm.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+// useForm - custom hook
+export const useForm = (initialState = {}) => {
+  const [inputValues, setInputValues] = useState(initialState);
+
+  
+  const resetForm = () => {
+    setInputValues(initialState);
+  };
+
+  const setForm = (newValues) => {
+    setInputValues(newValues);
+  };
+
+  const handleInputChange = ({ target }) => {
+    setInputValues({
+      ...inputValues,
+      [target.name]: target.value,
+    });
+  };
+
+  return { inputValues, handleInputChange, resetForm, setForm };
+};
